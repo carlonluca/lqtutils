d8bcc8c833b4da787ba9fe7af547b64dc7255276
Luca Carlon <carlon.luca@gmail.com>
2020-05-30 11:30:26 +0200

    Added tools to speed up implementaton of a settings file.

--- lqtutils/tst_lqtutils.cpp
+++ lqtutils/tst_lqtutils.cpp
@@ -24,6 +24,8 @@
 
 #include <QtTest>
 #include <QString>
+#include <QImage>
+#include <QPainter>
 
 #include "../lqtutils_prop.h"
 #include "../lqtutils_string.h"
@@ -38,23 +40,12 @@ public:
 		QObject(parent) {}
 };
 
-class LTestSettings : public QObject
-{
-    Q_OBJECT
-public:
-    static LTestSettings& instance() {
-        static LTestSettings _instance;
-        return _instance;
-    }
-
-    L_DEFINE_VALUE(QString, string1, QString("string1"), toString)
-
-protected:
-    LTestSettings(QObject* parent = nullptr) : QObject(parent) {}
-
-protected:
-    QSettings m_settings;
-};
+L_DECLARE_SETTINGS(LSettingsTest, new QSettings("settings.ini", QSettings::IniFormat))
+L_DEFINE_VALUE(QString, string1, QString("string1"), toString)
+L_DEFINE_VALUE(QSize, size, QSize(100, 100), toSize)
+L_DEFINE_VALUE(double, temperature, -1, toDouble)
+L_DEFINE_VALUE(QByteArray, image, QByteArray(), toByteArray)
+L_END_CLASS
 
 class LQtUtilsTest : public QObject
 {
@@ -83,9 +74,49 @@ void LQtUtilsTest::test_case1()
 
 void LQtUtilsTest::test_case2()
 {
+    QImage image(100, 100, QImage::Format_ARGB32);
+    QByteArray data;
+    QBuffer buffer(&data);
+    buffer.open(QIODevice::WriteOnly);
+    QPainter p(&image);
+    p.setBrush(Qt::red);
+    p.drawRect(image.rect());
+    image.save(&buffer, "jpg");
+    QVERIFY(data.size() != 0);
 
+    {
+        LSettingsTest& settings = LSettingsTest::instance();
+        settings.set_string1("some string");
+        settings.set_size(QSize(250, 200));
+        settings.set_temperature(36.7);
+        settings.set_image(data);
+    }
+
+    {
+        QSettings settings("settings.ini", QSettings::IniFormat);
+        QCOMPARE(settings.value(QSL("string1")), QSL("some string"));
+        QCOMPARE(settings.value(QSL("size")).toSize(), QSize(250, 200));
+        QCOMPARE(settings.value(QSL("temperature")).toDouble(), 36.7);
+        QCOMPARE(settings.value(QSL("image")).toByteArray(), data);
+    }
+
+    bool signalEmitted = false;
+    QEventLoop loop;
+    QTimer::singleShot(0, this, [] {
+        LSettingsTest::instance().set_size(QSize(1280, 720));
+    });
+    connect(&LSettingsTest::instance(), &LSettingsTest::sizeChanged, this, [&signalEmitted] {
+        QCOMPARE(LSettingsTest::instance().size(), QSize(1280, 720));
+        signalEmitted = true;
+    });
+    connect(&LSettingsTest::instance(), &LSettingsTest::sizeChanged,
+            &loop, &QEventLoop::quit);
+    loop.exec();
+
+    QCOMPARE(LSettingsTest::instance().size(), QSize(1280, 720));
+    QVERIFY(signalEmitted);
 }
 
-QTEST_APPLESS_MAIN(LQtUtilsTest)
+QTEST_MAIN(LQtUtilsTest)
 
 #include "tst_lqtutils.moc"
--- lsettings.h
+++ lsettings.h
@@ -4,18 +4,35 @@
 #include <QObject>
 #include <QSettings>
 
-#define L_DEFINE_VALUE(type, name, def, f)                                 \
-    public:                                                                \
-        type name() const { return m_settings.value(#name, def).f(); }     \
-    public Q_SLOTS:                                                        \
-        void setter(type value) {                                          \
-            if (name() == value) return;                                   \
-            m_settings.setValue(#name, value);                             \
-            emit name##Changed(value);                                     \
-        }                                                                  \
-    Q_SIGNALS:                                                             \
-        void name##Changed(type name);                                     \
-    private:                                                               \
-        Q_PROPERTY(type name READ name WRITE setter NOTIFY name##Changed)
+#define L_DEFINE_VALUE(type, name, def, f)                                    \
+    public:                                                                   \
+        type name() const { return m_settings->value(#name, def).f(); }       \
+    public Q_SLOTS:                                                           \
+        void set_##name(type value) {                                         \
+            if (name() == value) return;                                      \
+            m_settings->setValue(#name, value);                               \
+            emit name##Changed(value);                                        \
+        }                                                                     \
+    Q_SIGNALS:                                                                \
+        void name##Changed(type name);                                        \
+    private:                                                                  \
+        Q_PROPERTY(type name READ name WRITE set_##name NOTIFY name##Changed)
+
+#define L_DECLARE_SETTINGS(classname, qsettings) \
+    class classname : public QObject \
+    { \
+        Q_OBJECT \
+    public: \
+        static classname& instance() { \
+            static classname _instance; \
+            return _instance;            \
+        } \
+    private: \
+        classname(QObject* parent = nullptr) : QObject(parent) { \
+            m_settings = qsettings; \
+        } \
+        ~classname() { delete m_settings; } \
+    protected: \
+        QSettings* m_settings;
 
 #endif
