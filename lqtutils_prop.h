abd4202780510c090b48c13247468a14319f816d
Luca Carlon <carlon.luca@gmail.com>
2020-11-21 12:19:42 +0100

    Implemented prop generation for gadgets.

--- lqtutils_prop.h
+++ lqtutils_prop.h
@@ -29,6 +29,10 @@
 // https://stackoverflow.com/questions/5134523/msvc-doesnt-expand-va-args-correctly
 #define EXPAND( x ) x
 #define L_PROP_GET_MACRO(_1, _2, _3, _4, NAME,...) NAME
+
+// QObject
+// =======
+
 #define L_RW_PROP(...) \
     EXPAND( L_PROP_GET_MACRO(__VA_ARGS__, L_RW_PROP4, L_RW_PROP3, L_RW_PROP2)(__VA_ARGS__) )
 #define L_RO_PROP(...) \
@@ -91,5 +95,59 @@
     private:
 
 #define L_END_CLASS };
+
+// Gadget
+// ======
+
+#define L_RW_GPROP(...) \
+    EXPAND( L_PROP_GET_MACRO(__VA_ARGS__, L_RW_GPROP4, L_RW_GPROP3, L_RW_GPROP2)(__VA_ARGS__) )
+#define L_RO_GPROP(...) \
+    EXPAND( L_PROP_GET_MACRO(__VA_ARGS__, L_RO_GPROP4, L_RO_GPROP3, L_RO_GPROP2)(__VA_ARGS__) )
+
+// A read-write prop both in C++ and in QML.
+#define L_RW_GPROP_(type, name, setter)                                    \
+    public:                                                                \
+        type name() const { return m_##name ; }                            \
+        void setter(type name) {                                           \
+            if (m_##name == name) return;                                  \
+            m_##name = name;                                               \
+        }                                                                  \
+    private:                                                               \
+        Q_PROPERTY(type name READ name WRITE setter);
+
+#define L_RW_GPROP4(type, name, setter, def)                                \
+    L_RW_GPROP_(type, name, setter)                                         \
+    type m_##name = def;
+
+#define L_RW_GPROP3(type, name, setter)                                     \
+    L_RW_GPROP_(type, name, setter)                                         \
+    type m_##name;
+
+// A read-write prop from C++, but read-only from QML.
+#define L_RO_GPROP_(type, name, setter)                       \
+    public:                                                   \
+        type name() const { return m_##name ; }               \
+        void setter(type name) {                              \
+            if (m_##name == name) return;                     \
+            m_##name = name;                                  \
+        }                                                     \
+    private:                                                  \
+        Q_PROPERTY(type name READ name);
+
+#define L_RO_GPROP4(type, name, setter, def)                  \
+    L_RO_GPROP_(type, name, setter)                           \
+    type m_##name = def;
+
+#define L_RO_GPROP3(type, name, setter)                       \
+    L_RO_GPROP_(type, name, setter)                           \
+    type m_##name;
+
+// Convenience macros to speed up a QObject subclass.
+#define L_BEGIN_GADGET(name)                          \
+    class name                                        \
+    {                                                 \
+        Q_GADGET
+
+#define L_END_GADGET };
 
 #endif // LQTUTILS_PROP_H
